b0cc8ae12a258ce4b9a5ee0e4423176a69ed0d13
alvarezfelipedev <n.felialvarez@gmail.com>
2024-03-10 23:02:26 -0300

    ventajas de nuestra gestoria finalizado, iniciado servicios como gestor automotor

---
+++ src/Components/ServicioAutomotor.jsx
@@ -0,0 +1,77 @@
+function ServicioAutomotor() {
+    const features = [
+        {
+            title: "Patentamientos y Transferencias",
+        },
+        {
+            title: "Informes y Certificados de Dominio",
+        },
+        {
+            title: "Altas, bajas para Autos, Motos ante el R.N",
+        },
+        {
+            title: "Duplicado de Título",
+        },
+        {
+            title: "Duplicado de Patente",
+        },
+        {
+            title: "Autorizado a conducir (Cédula Azul)",
+        },
+        {
+            title: "Inscripción y Cancelación de Prendas",
+        },
+        {
+            title: "Renovación o Duplicado de Cédula Verde",
+        },
+        {
+            title: "Cambios de Uso",
+        },
+        {
+            title: "Deudas de Patentes de Infacciones",
+        },
+        {
+            title: "Denuncia de Venta",
+        },
+        {
+            title: "Y otros trámites más generales",
+        }
+    ]
+
+    return (
+        <>
+            <section className="py-14">
+                <div className="max-w-screen-xl mx-auto px-4 text-center text-gray-600 md:px-8">
+                    <div className="max-w-2xl mx-auto">
+                        <h3 className="text-gray-800 text-2xl font-semibold sm:text-3xl">
+                            Servicios como Gestor Automotor
+                        </h3>
+                    </div>
+                    <div className="mt-12">
+                        <ul className="grid gap-y-8 gap-x-8 sm:grid-cols-2 lg:grid-cols-2 mx-auto">
+                            {
+                                features.map((item, idx) => (
+                                    <li key={idx} className="flex">
+                                        <div className="flex items-center gap-x-4">
+                                            <div className="flex-none w-12 h-12 bg-indigo-50 text-indigo-600 rounded-full flex items-center justify-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                                </svg>
+                                            </div>
+                                            <h4 className="my-2 text-lg text-gray-800 font-semibold">
+                                                {item.title}
+                                            </h4>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </>
+
+    )
+}
+
+export default ServicioAutomotor;
---
+++ src/Components/VentajasGestoria.jsx
@@ -0,0 +1,74 @@
+function VentajasGestoria() {
+    const features = [
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                </svg>,
+            title: "Ahorro de tiempo y esfuerzo",
+            desc: "Olvídate de las largas filas, la búsqueda de formularios y la presentación de documentación. Nos encargamos de todo, liberándote para que puedas dedicar tu tiempo a otras actividades."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                </svg>,
+            title: "Seguridad jurídica",
+            desc: "Te protegemos de posibles fraudes o estafas, verificando la documentación y el estado legal del vehículo antes de realizar cualquier transacción."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+                </svg>,
+            title: "Asesoramiento personalizado",
+            desc: "Te brindamos información clara y precisa sobre tus derechos y obligaciones como titular de un vehículo, y te asesoramos en la toma de decisiones relacionadas con la gestión del mismo."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+                </svg>,
+            title: "Experiencia y conocimiento",
+            desc: "Estamos capacitados y actualizados en las últimas normas y procedimientos del sector automotor. Sabemos cómo realizar cada trámite de forma rápida y eficiente, evitando errores y demoras."
+        },
+
+    ]
+
+    return (
+        <section className="py-14">
+            <div className="max-w-screen-xl mx-auto px-4 text-center text-gray-600 md:px-8">
+                <div className="max-w-2xl mx-auto">
+                    <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                        Ventajas de nuestra Gestoría
+                    </h3>
+                    <p className="mt-3">
+                        Si estás buscando un gestor automotor de confianza, no dudes en contactarme. Te ofreceré un servicio personalizado y eficiente, con la garantía de un profesional matriculado.
+                        Con Matricula bajo el DNRPA y años de experiencia en el rubro.
+                    </p>
+                </div>
+                <div className="mt-12">
+                    <ul className="grid gap-y-8 gap-x-12 sm:grid-cols-2 lg:grid-cols-2">
+                        {
+                            features.map((item, idx) => (
+                                <li key={idx} className="space-y-3">
+                                    <div className="w-12 h-12 mx-auto bg-indigo-50 text-indigo-600 rounded-full flex items-center justify-center">
+                                        {item.icon}
+                                    </div>
+                                    <h4 className="text-lg text-gray-800 font-semibold">
+                                        {item.title}
+                                    </h4>
+                                    <p>
+                                        {item.desc}
+                                    </p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default VentajasGestoria;
